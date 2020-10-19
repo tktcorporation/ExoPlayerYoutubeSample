c67d0dbe2a72858303263ddf10482275dd9c09b4
tktcorporation <tktcorporation.go@gmail.com>
2020-10-20 02:55:53 +0900

    refactored

--- app/src/main/java/com/example/countup/MainActivity.kt
+++ app/src/main/java/com/example/countup/MainActivity.kt
@@ -3,40 +3,29 @@ package com.example.countup
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.countup.domain.Greeting
+import com.example.countup.domain.Sheeps
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var sheepCount = 0
+    val sheeps = Sheeps()
     var mp: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var greeting = "こんにちは"
-        val trialTime = Date()
-        val calendar = GregorianCalendar()
-        calendar.time = trialTime
-
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
-        if (hour in 1..11) {
-            greeting = "おはよう"
-        } else if (15 < hour) {
-            greeting = "こんばんは"
-        }
-
-        textview.text = greeting
+        textview.text = Greeting(Date()).comment
 
         mp = MediaPlayer.create(applicationContext, R.raw.cry)
 
         rootLayout.setOnClickListener {
-            sheepCount++
-            val sheepText = "ひつじが $sheepCount 匹"
-            textview.text = sheepText
+            sheeps.add()
+            textview.text = sheeps.getCountText()
 
-            when(sheepCount % 2) {
-                0 -> {
+            when(sheeps.isStanding()) {
+                true -> {
                     imageView1.setImageResource(R.drawable.sheep_sleeping)
                     mp?.start()
                 }
