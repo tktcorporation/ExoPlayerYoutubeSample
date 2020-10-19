de8e9dab2d8a13a5de4e72dd123bb7078b57009c
tktcorporation <tktcorporation.go@gmail.com>
2020-10-20 01:45:58 +0900

    音が出るように

--- app/src/main/java/com/example/countup/MainActivity.kt
+++ app/src/main/java/com/example/countup/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.countup
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,6 +9,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     var sheepCount = 0
+    var mp: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +28,18 @@ class MainActivity : AppCompatActivity() {
 
         textview.text = greeting
 
+        mp = MediaPlayer.create(applicationContext, R.raw.cry)
+
         rootLayout.setOnClickListener {
             sheepCount++
             val sheepText = "ひつじが $sheepCount 匹"
             textview.text = sheepText
 
             when(sheepCount % 2) {
-                0 -> imageView1.setImageResource(R.drawable.sheep_sleeping)
+                0 -> {
+                    imageView1.setImageResource(R.drawable.sheep_sleeping)
+                    mp?.start()
+                }
                 else -> imageView1.setImageResource(R.drawable.sheep_standing)
             }
         }
