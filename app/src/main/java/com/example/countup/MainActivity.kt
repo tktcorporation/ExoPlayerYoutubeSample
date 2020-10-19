6d77d9e3ea6463234a9a534bad40bc89820127ba
tkt <tktcorporation.go@gmail.com>
2020-10-19 23:03:28 +0900

    import kotlinx.android.synthetic.main.activity_main.* を追加してテキストの変更処理

--- app/src/main/java/com/example/countup/MainActivity.kt
+++ app/src/main/java/com/example/countup/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.countup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var greeting = "こんにちは"
+        val trialTime = Date()                         //追加
+        val calendar = GregorianCalendar()             //追加
+        calendar.time = trialTime                      //追加
+
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)  //追加
+        if (hour in 1..11) {                           //追加
+            greeting = "おはよう"                       //追加
+        } else if (15 < hour) {                        //追加
+            greeting = "こんばんは"                     //追加
+        }                                              //追加
+
+        textview.text = greeting
     }
 }
