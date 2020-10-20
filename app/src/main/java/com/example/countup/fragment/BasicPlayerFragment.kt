e246a8af2c1b3a755d33b738e54b6dafe3663801
tktcorporation <tktcorporation.go@gmail.com>
2020-10-20 09:50:23 +0900

    fix deprecated codes

--- app/src/main/java/com/example/countup/fragment/BasicPlayerFragment.kt
+++ app/src/main/java/com/example/countup/fragment/BasicPlayerFragment.kt
@@ -8,20 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.countup.R
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
-import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util.getUserAgent
 import kotlinx.android.synthetic.main.fragment_player.*
 
 class BasicPlayerFragment(private val ctx: Context, private val uri : Uri): Fragment() {
-
-    private lateinit var playerView: PlayerView
-
     private lateinit var exoPlayer: ExoPlayer
 
     override fun onCreateView(
@@ -38,14 +31,9 @@ class BasicPlayerFragment(private val ctx: Context, private val uri : Uri): Frag
         exoPlayer = SimpleExoPlayer.Builder(ctx).build()
         exoPlayer.repeatMode = Player.REPEAT_MODE_ALL
         exoPlayer.playWhenReady = true
-        exoPlayer.apply {
-            prepare(buildMediaSource())
-        }
-
-        playerView = player_view
-        playerView.apply {
-            player = exoPlayer
-        }
+        exoPlayer.setMediaSource(buildMediaSource())
+        exoPlayer.prepare()
+        player_view.player = exoPlayer
     }
 
     private fun createDataSource(): DefaultDataSourceFactory {
@@ -58,6 +46,6 @@ class BasicPlayerFragment(private val ctx: Context, private val uri : Uri): Frag
     private fun buildMediaSource(): HlsMediaSource {
         return HlsMediaSource
             .Factory(createDataSource())
-            .createMediaSource(uri)
+            .createMediaSource(MediaItem.fromUri(uri))
     }
 }
