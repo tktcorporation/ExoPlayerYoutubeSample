8430bfe0aee14653256251a15611bed90137d827
tkt <tktcorporation.go@gmail.com>
2020-10-23 05:20:19 +0900

    refactor

--- app/src/main/java/com/example/exoplayeryoutubesample/infrastructure/http/YoutubeHttp.kt
+++ app/src/main/java/com/example/exoplayeryoutubesample/infrastructure/http/YoutubeHttp.kt
@@ -3,20 +3,17 @@ package com.example.exoplayeryoutubesample.infrastructure.http
 import android.net.Uri
 import android.util.Log
 import com.example.exoplayeryoutubesample.domain.YoutubeInfo
-import okhttp3.OkHttpClient
 import okhttp3.Request
 
-object HttpClient {
-    val instance = OkHttpClient()
-}
+const val MAC_SAFARI_USER_AGENT: String = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"
 
-class YoutubeHttp() {
+class YoutubeHttp(private val client: HttpClient) {
     // Function that makes the network request, blocking the current thread
-    fun fetchM3U8Url(): String {
-        val client = HttpClient.instance
+    fun fetchM3U8Url(id: String): String {
+        val client = client.instance
         val request = Request.Builder()
-            .url("https://www.youtube.com/get_video_info?video_id=rvkxtVkvawc")
-            .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15")
+            .url("https://www.youtube.com/get_video_info?video_id=$id")
+            .header("User-Agent", MAC_SAFARI_USER_AGENT)
             .build()
 
         val response = client.newCall(request).execute()
@@ -24,7 +21,7 @@ class YoutubeHttp() {
         Log.d("TAG", body.toString())
         if (body == null) {
             Log.e("body == null", "onViewCreated: received body is null.")
-            body = "sss"
+            throw Error("it couldn't fetch a youtube url.")
         }
         return Uri.decode(YoutubeInfo(body).extractM3U8URL())
     }
